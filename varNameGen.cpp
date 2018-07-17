1f61c733a9dca3d2b8f0c6fe8f56856bd4f693f4
John Bedette <jbedette@quizor1.cs.pdx.edu>
2018-07-17 02:35:37 -0700

    end of 1st draft

--- varNameGen.cpp
+++ varNameGen.cpp
@@ -4,32 +4,34 @@
 using namespace std;
 
 //John Bedette, cs162
-//a simple variable generator
+//a simple variable name generator
 
 
 //consts
-int KEYWORDS = 41;
+const int KEYWORDS = 41;
+const int i = 0;
 
 //prototypes
 void welcome();
 void inputKeywords(char[]);
-void selectOutput(char[]);
+void selectOutput(char[], char);
 void removeSpaces(char[]);
 void toUppercase(char[]);
 void toLowercase(char[]);
-void toCamelcase(char[]);
+void toCamelCase(char[]);
 void toUnderscore(char[]);
 
 //main
 int main(){
     //variables
     char keywords[KEYWORDS];
+    char option = ' ';
     
     //main flow
     welcome();
     inputKeywords(keywords);
     cout << ">>> " << keywords;
-    selectOutput(keywords);
+    selectOutput(keywords, option);
     return 0;
 }
 
@@ -43,13 +45,13 @@ void inputKeywords(char keywords[]){
     cin.ignore(100,'\n');
 }
 //present user with menu and select string modification
-void selectOutput(char keywords[]){
-    char option = ' ';
+void selectOutput(char keywords[],char option){
     cout << "\n\n" << ">>> Please select an output option:" << '\n';
-    cout << ">>> [L]owercase" << "\n";
-    cout << ">>> [U]ppercase" << "\n";
-    cout << ">>> [C]amelcase" << "\n";
-    cout << ">>> [S]eparated by underscores" << "\n\n";
+    cout << ">>> [L]owercase" << '\n';
+    cout << ">>> [U]ppercase" << '\n';
+    cout << ">>> [C]amelCase with the first letter capitalized" << '\n';
+    cout << ">>> [R]egular CamelCase" << '\n';
+    cout << ">>> [S]eparated by Underscores" << "\n\n";
 
     cin >> option;
     option = toupper(option);
@@ -57,34 +59,76 @@ void selectOutput(char keywords[]){
 
     if(option == 'L'){
         toLowercase(keywords);
-    }else if(option == 'U'){
-        toUppercase(keywords);
+        removeSpaces(keywords);
+        cout << "\n\n" << ">>> Your varible name: " << keywords << "<<<" << '\n';
     }else if(option == 'C'){
-        toCamelcase(keywords);
+        toCamelCase(keywords);
+        removeSpaces(keywords);
+        keywords[0] = toupper(keywords[0]);
+        cout << "\n\n" << ">>> Your varible name: " << keywords << "<<<" << '\n';
+    }else if(option == 'R'){
+        toCamelCase(keywords);
+        removeSpaces(keywords);
+        keywords[0] = tolower(keywords[0]);
+        cout << "\n\n" << ">>> Your varible name: " << keywords << "<<<" << '\n';
     }else if(option == 'S'){
         toUnderscore(keywords);
+        toLowercase(keywords);
+        cout << "\n\n" << ">>> Your varible name: " << keywords << "<<<" << '\n';
+    }else if(option == 'U'){
+        toUppercase(keywords);
+        cout << "\n\n" << ">>> Your varible name: " << keywords << "<<<" << '\n';
     }else{
-        //wonder how this works
-       //selectOutput(option, keywords); 
+        //if the user doesn't pick anything this kicks you back to selection menu
+        selectOutput(keywords, option); 
     }
 }
 //remove spaces in toUpper, toLower, toCamel
-void removeSpaces(char keywords[]){
-
+void removeSpaces(char keywords[]){ 
+    int length = strlen(keywords);
+    int i = 0;
+    for(i = length; i > 0; --i){
+       if (keywords[i] == ' '){
+           int iTwo = i;
+           for(i = iTwo; i < length; ++i){
+               keywords[i] = keywords[(i+1)];
+           } 
+       }
+    } 
 }
 //slice out spaces, then to upper
-void toUppercase(char keywords[]){
-
+void toCamelCase(char keywords[]){
+    int length = strlen(keywords);
+    int i = 0;
+    for(i = length; i > 0; --i){
+        if(keywords[i] == ' '){
+            keywords[(i+1)] = toupper(keywords[(i+1)]);
+        }
+    }
 }
 //slice out spaces, then to lower
 void toLowercase(char keywords[]){
-
+    int length = strlen(keywords);
+    int i = 0;
+    for(i = 0; i < length; ++i){
+        keywords[i] = tolower(keywords[i]);
+    }
 }
-//capitalize first letters of each word, then slice out spaces
-void toCamelcase(char keywords[]){
-
+//capitalize the entire word
+void toUppercase(char keywords[]){
+    int length = strlen(keywords);
+    int i = 0;
+    for(i = 0; i < length; ++i){
+        keywords[i] = toupper(keywords[i]);
+    }
 }
 //set to lower, then replace spaces w/underscores
 void toUnderscore(char keywords[]){
-
+    int length = strlen(keywords);
+    int i = 0;
+    for(i = length; i > 0; --i){
+       if (keywords[i] == ' '){
+            keywords[i] = '_';
+       }
+    } 
 }
